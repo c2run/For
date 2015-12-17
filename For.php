fa70c639bd46e4cb6a9d8f663edeca5be6cefe4f
C2RUN <jose.cerda66@gmail.com>
2015-12-17 14:48:32 -0300

    Primeros cambios
    Ejemplo bucle FOR en PHP

---
+++ For.php
@@ -0,0 +1,13 @@
+<?php
+//Ejemplo bucle FOR
+
+  //parametros del bucle
+  //inicio del bucle en 0
+  //limite del bucle menor o igual a 5
+  //incremento de la variable en 1
+  for($x=0;$x<=5;$x++)
+  {
+  echo "El número es: $x <br>";
+  }
+
+?>
